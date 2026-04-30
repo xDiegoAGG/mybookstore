abb8b4e479784d39a21c8cc58879d146a40c9b6a
Diego Gonzalez <142106783+xDiegoAGG@users.noreply.github.com>
2026-04-30 02:23:41 -0500

    fixes at nginx conf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api/books/': {
-        target: 'http://localhost:5001',
+        target: 'http://localhost:3003',
         changeOrigin: true,
       }
     }
